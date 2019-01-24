8dde63695a3a201ab651accae614b6f2556187c8
hibozzy18 <hknguruwe@gmail.com>
2019-01-24 11:26:46 +0100

    correct input file dependences

--- include/input.h
+++ include/input.h
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include <data_structure.h>
-#include<utilities_lmjd.h>
+//#include<utilities_lmjd.h>
 
 #define BLEN 200
 
 static int get_a_line(FILE *fp, char *buf);
 void populate_data(FILE *frestart, char line[], char restfile[],\
 char trajfile[], char ergfile[], mdsys_t *sys);
-void readRestart(File *f);
+void readRestart(FILE *f);
