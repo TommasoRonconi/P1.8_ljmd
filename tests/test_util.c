fe69eb5c39f5265db34226018720ddce378a8154
zampierimatteo91 <zampierimatteo.91@gmail.com>
2019-01-25 12:16:26 +0100

    Hopefully embellished test for utilities

--- tests/test_util.c
+++ tests/test_util.c
@@ -12,12 +12,10 @@ int main()
 {
 	
 	/* Define different sizes for the arrays */
-	const int size1 = 100;
-	const int size2 = 70;
-	int size_new;
+	const int size1 = 10;
 
-	/* Declare arrays to be tested */
-	double *test_array1, *test_array2;
+	/* Declare array to be tested qith azzero() */
+	double *test_array1;
 	
 	/* Declare variable to test pbc() */
 	double xx;
@@ -26,36 +24,30 @@ int main()
 	/*---------- azzero() --------------*/
 	/* Allocate memory */
 	test_array1 = (double*) malloc(size1 * sizeof(*test_array1));
-	test_array2 = (double*) malloc(size1 * sizeof(*test_array2));
-
+	
+	printf("/--------- Testing for azzero() ---------/\n\n");
+	printf("Initialize the array...\nPrint the array:\n\n");
 	/* Initialize arrays */
 	for(int ii = 0; ii < size1; ii++)
 	{
 		test_array1[ii] = ii * 2.0;
+		printf("%f ", test_array1[ii]);
 	}
+	printf("\n\nZero out the array with azzero()...\nCheck the array:\n\n");
 
-	for(int jj = 0; jj < size1; jj++)
-	{
-		test_array2[jj] = jj * 3.0;
-	}
-
-	/* Zero out the arrays
-		azzero(test_array1, size1) works properly
-		azzero(test_array2, size2) rightly gives error */
+	/* Zero out the array with
+		azzero(test_array1, size1) */
 	azzero(test_array1, size1);
-
-	/*azzero(test_array2, size1);*/
-
-	azzero(test_array2, size1);
 
 	for(int ii = 0; ii < size1; ++ii)
 	{
 		assert(test_array1[ii] == 0);
-		assert(test_array2[ii] == 0);
-	}	
+		printf("%f ", test_array1[ii]);
+	}
+	printf("\n\n");
 
-	free(test_array2);
 	free(test_array1);
+
 
 	/*---------- pbc() ---------------*/
 	xx = 600.0;
@@ -66,17 +58,18 @@ int main()
 		Trying assert with the correct and
 		an incorrect value */
 	
-	printf("assert for correct value of xx(=200.0)\n");
+	printf("/--------- Testing for pbc() ---------/\n\n");
+	printf("Assert for correct value of xx(=200.0)\n");
 	assert(xx == 200.0);
 	printf("--------------------------------------\n");
 
-	printf("Computed value of xx = %f\n", xx);
+	printf("Check: computed value of xx = %f\n", xx);
 
-	printf("--------------------------------------\n");
+	/*printf("--------------------------------------\n");
 
 	printf("assert for incorrect value of xx (=300.0)\n");
 	assert(xx == 300.0);
-	printf("--------------------------------------\n");
+	printf("--------------------------------------\n");*/
 		
 	return 0;
 }
