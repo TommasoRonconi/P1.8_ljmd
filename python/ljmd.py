b197fc19419c827a52c8df06fff03873c9508f22
hibozzy18 <hknguruwe@gmail.com>
2019-01-27 17:43:38 +0100

    add array creation and create a test object to pass to  dynamic library

--- python/ljmd.py
+++ python/ljmd.py
@@ -41,3 +41,53 @@ def read_rest(filename, start, end):
             z.append(float(values[2]))
     f.closed
     return x, y, z
+
+
+#return data from file to lists
+rx, ry, rz = read_rest("data/argon_108.rest", r_start, r_end)
+vx, vy, vz = read_rest("data/argon_108.rest", v_start, v_end)
+
+#element size
+elem_size = c_double*num_atoms
+
+#create empty elements
+array_rx = elem_size()
+array_ry = elem_size()
+array_rz = elem_size()
+array_vx = elem_size()
+array_vy = elem_size()
+array_vz = elem_size()
+
+#load vectors from th r file
+array_rx = get_array(rx, array_rx)
+array_ry = get_array(ry, array_ry)
+array_rz = get_array(rz, array_rz)
+array_vx = get_array(vx, array_vx)
+array_vy = get_array(vy, array_vy)
+array_vz = get_array(vz, array_vz)
+
+# print(array_rx[0])
+#populate  data of a data structure
+#and return the mdsys
+
+# print(list)
+
+
+# mdsys = _mdsys(natoms=108, nfi=100, nsteps=1000, mass=600, epsilon=200, sigma=78, rcut=90.00, box=500,
+#  dt = 8.9, ekin = 0.0, epot = 0.0, temp = 0.0, rx=array_rx,
+# ry = array_ry, rz=array_rz, vx=array_vx, vy=array_vy, vz=array_vz)
+mdsys = _mdsys(natoms=int(list[0]), mass=list[1], epsilon=list[2], sigma=list[3], rcut=list[4], box=list[5],
+               nsteps=int(list[9]), dt=list[10], nfi=0, ekin=0.0, epot=0.0, temp=0.0, rx=array_rx,
+               ry=array_ry, rz=array_rz, vx=array_vx, vy=array_vy, vz=array_vz)
+
+
+print(mdsys.natoms)
+print(mdsys.mass)
+print(mdsys.epsilon)
+print(mdsys.sigma)
+print(mdsys.rcut)
+print(mdsys.box)
+print(mdsys.nsteps)
+print(mdsys.dt)
+print(mdsys.rx[0])
+dso.test(byref(mdsys))
