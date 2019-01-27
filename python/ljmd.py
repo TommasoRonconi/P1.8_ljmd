f302ca874d66d3fa528c8635a22e7c968e8986e6
hibozzy18 <hknguruwe@gmail.com>
2019-01-27 17:42:12 +0100

    add read_inp and read_rest functions

--- python/ljmd.py
+++ python/ljmd.py
@@ -11,3 +11,33 @@ dso = CDLL("./lmjd.so")
 class _mdsys(Structure):
     _fields_ = [("natoms", c_int), ("nfi", c_int), ("nsteps", c_int), ("dt", c_double), ("mass", c_double),  ("epsilon", c_double),  ("sigma", c_double), ("box", c_double), ("rcut", c_double), ("ekin", c_double), ("epot", c_double),
                 ("temp", c_double), ("rx", POINTER(c_double)), ("ry", POINTER(c_double)), ("rz", POINTER(c_double)), ("vx", POINTER(c_double)), ("vy", POINTER(c_double)), ("vz", POINTER(c_double))]
+
+
+#pass object type
+dso.test.argtypes = [POINTER(_mdsys)]
+
+#Read from input file
+def read_inp(filename):
+    list = []
+    with open(filename, 'r') as f:
+        for line in f:
+            values = [s for s in line.split()]
+            try:
+                list.append(float(values[0]))
+            except ValueError:
+                list.append(values[0])
+    f.closed
+    return list
+
+#Read from rest file
+def read_rest(filename, start, end):
+    from itertools import islice
+    x, y, z = [], [], []
+    with open(filename, 'r') as f:
+        for line in islice(f, start, end):
+            values = [float(s) for s in line.split()]
+            x.append(float(values[0]))
+            y.append(float(values[1]))
+            z.append(float(values[2]))
+    f.closed
+    return x, y, z
