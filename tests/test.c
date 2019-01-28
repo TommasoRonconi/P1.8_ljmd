c7520d1a2c03ff4483623d7f9f890b7946d325dd
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2019-01-28 14:06:26 +0100

    set-up of test.c to work with new serial version, now working and passing tests

--- tests/test.c
+++ tests/test.c
@@ -10,6 +10,9 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <math.h>
+#ifdef USE_MPI
+#include <mpi.h>
+#endif //USE_MPI
 #include <utilities_ljmd.h>
 #include <data_structure.h>
 #include <compute_force.h>
@@ -24,7 +27,7 @@ int tests_run = 0;
 test_sys gsys;
 
 /* main */
-int main( /* int argc, char **argv */ ) 
+int main( int argc, char **argv ) 
 {
 
   int nprint;
@@ -32,7 +35,16 @@ int main( /* int argc, char **argv */ )
   FILE *fp,*traj,*erg;
   mdsys_t sys;
 
-  if ( populate_data(stdin, &line, &restfile, &trajfile, &ergfile, &sys, &nprint/* , BLEN */) ) return 1;
+  /* Initialize */
+  initialize( argc, argv, &sys );
+
+  /* populate the data_structure with input data */
+  if ( populate_data( stdin, &line, &restfile, &trajfile, &ergfile, &sys, &nprint ) ) {
+#ifdef USE_MPI
+    MPI_Abort( MPI_COMM_WORLD, 1 );
+#endif //USE_MPI
+    return 1;
+  }
   
   // allocate_sys_arrays( gsys );
   allocate_sys_arrays( &sys );
@@ -57,8 +69,12 @@ int main( /* int argc, char **argv */ )
   gsys.pbc_t = xx;
 
   /* read restart */
-  fp = fopen(restfile, "r");
-  readRestart(fp,&sys,restfile);
+  if ( readRestart( &sys, restfile ) ) {
+#ifdef USE_MPI
+    MPI_Abort( MPI_COMM_WORLD, 2 );
+#endif //USE_MPI
+    return 2;
+  }
 
   /* initialize forces and energies.*/
   sys.nfi=0;
@@ -66,62 +82,77 @@ int main( /* int argc, char **argv */ )
   ekin(&sys);
     
 /* ======= */
-    erg=fopen(ergfile,"w");
-    traj=fopen(trajfile,"w");
 
+  if ( sys.rank == 0 ) {
+    
+    /* Open the output files for energies and trajectories */
+    erg = fopen( ergfile, "w" );
+    traj = fopen( trajfile, "w" );
+
+    /* Outputting on the defined files */
     printf("Starting simulation with %d atoms for %d steps.\n",sys.natoms, sys.nsteps);
     printf("     NFI            TEMP            EKIN                 EPOT              ETOT\n");
     output(&sys, erg, traj);
+    
+  }
 
-    /**************************************************/
-    /* main MD loop */
-    for(sys.nfi=1; sys.nfi <= sys.nsteps; ++sys.nfi) {
+  /**************************************************/
+  /* main MD loop */
+  for(sys.nfi=1; sys.nfi <= sys.nsteps; ++sys.nfi) {
 
-        /* write output, if requested */
-        if ((sys.nfi % nprint) == 0)
-            output(&sys, erg, traj);
+    /* write output, if requested */
+    if ( ( sys.rank == 0 ) && ( ( sys.nfi % nprint ) == 0 ) ) output(&sys, erg, traj);
 
+    /* propagate system and recompute energies by one half step*/          
+    velverlet_first_half(&sys);
+
+    /* compute forces and potential energy */
+    force(&sys);
+
+    /* propagate system and recompute energies by another half step*/  
+    velverlet_second_half(&sys); 
         
-        velverlet_first_half(&sys); /* propagate system and recompute energies by one half step*/  
-    	force(&sys); /* compute forces and potential energy */
-        velverlet_second_half(&sys); /* propagate system and recompute energies by another half step*/  
-        
-		if (sys.nfi == 10)
-		{
-			// test_kinetic_energy = sys.ekin;
-			// test_force = sys.fx[0];
-			// test_rx = sys.rx[0];
-			gsys.ekin = sys.ekin;
-			gsys.fx = sys.fx[0];
-			gsys.rx = sys.rx[0];
-		}
-        
-        ekin(&sys);
+    if (sys.nfi == 10)
+      {
+	// test_kinetic_energy = sys.ekin;
+	// test_force = sys.fx[0];
+	// test_rx = sys.rx[0];
+	gsys.ekin = sys.ekin;
+	gsys.fx = sys.fx[0];
+	gsys.rx = sys.rx[0];
+      }
 
+    /* Update kinetic energy */
+    ekin(&sys);
+    
+  }
+  /**************************************************/
 
-    }
-    /**************************************************/
-    /* clean up: close files, free memory */
+  /* clean up: close files, free memory */
+  if ( sys.rank == 0 ) {
+    printf("Simulation Done.\n");
     fclose(erg);
     fclose(traj);
+  }
 
-  	free_sys_arrays( &sys );
-  	// free_sys_arrays( gsys );
-    
-    printf("Simulation Done.\n");
-    /****************************************************/
-    /* Unittesting */
-	char *result = all_tests();
-	if (result != 0) 
-	{
-		 printf("%s\n", result);
-	}
-	else
-	{
-		 printf("\n\nALL TESTS PASSED\n\n");
-	}
-	printf("\nTests run: %d\n", tests_run);
+  /* free memory allocated */
+  free_sys_arrays( &sys );
+  
+  /****************************************************/
+  /* Unittesting */
+  char *result = all_tests();
+  if (result != 0) 
+    {
+      printf("%s\n", result);
+    }
+  else
+    {
+      printf("\n\nALL TESTS PASSED\n\n");
+    }
+  printf("\nTests run: %d\n", tests_run);
 
+  /* Finalize MPI */
+  finalize();
 
-    return 0;
+  return 0;
 }
