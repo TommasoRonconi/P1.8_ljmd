8cca263681192435acb3441a79e896b1917e0d6c
zampierimatteo91 <zampierimatteo.91@gmail.com>
2019-01-29 18:17:14 +0100

    added graph for scaling

---
+++ benchmarking/job_scaling_108.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#PBS -S /bin/bash
+#PBS -N scaling_omp
+#PBS -l nodes=1:ppn=20
+#PBS -l walltime=01:00:00
+#PBS -q reserved2
+#PBS -o /dev/null
+#PBS -e what.e
+
+cd $PBS_O_WORKDIR
+cd ../
+
+for nthreads in 1 2 3 4 5 6 7 8 10 12 18 19; do
+
+	for runs in 1..10; do
+	    
+		make
+		export OMP_NUM_THREADS=${nthreads}
+		make check 2>>times_omp.dat
+		make clean
+
+	done
+
+done
+
+		
