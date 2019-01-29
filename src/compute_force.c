165a57242e73fdbae4a0216c5c3962fad9406d32
jesusEV <jesus.espinoza.val@gmail.com>
2019-01-29 17:06:18 +0100

    expensive math removed, third law applied

--- src/compute_force.c
+++ src/compute_force.c
@@ -1,5 +1,36 @@
 #include <compute_force.h>
 
+/*Using Third Law of Newton*/
+#ifdef THIRD_LAW
+#define J_BEGIN() i+1	
+#define HALF_FACTOR() 2
+#else
+#define J_BEGIN() 0
+#define HALF_FACTOR() 1
+#endif
+
+/*Avoiding expensive math*/ 
+#ifdef AVOID_MATH
+#define FFAC() (12.0*c12*r6 - 6.0*c6)*r6*rinv
+#define EPOT() 0.5*r6*(c12*r6 - c6)
+#define  C12() 4.0*sys->epsilon*pow(sys->sigma,12.0)
+#define  C6()  4.0*sys->epsilon*pow(sys->sigma,6.0)
+#define  RRC() sys->rcut * sys->rcut
+#define RR() rx*rx + ry*ry + rz*rz
+#define R() sqrt(rr)
+#define RINV() 1.0/r
+#define R6() rinv*rinv*rinv*rinv*rinv*rinv
+#else
+#define FFAC() -4.0*sys->epsilon*(-12.0*pow(sys->sigma/rr,12.0)/rr + 6*pow(sys->sigma/rr,6.0)/rr)
+#define EPOT() 0.5*4.0*sys->epsilon*(pow(sys->sigma/rr,12.0) - pow(sys->sigma/rr,6.0))
+#define  C12() 0
+#define  C6() 0
+#define  RRC() sys->rcut
+#define RR() sqrt(rx*rx + ry*ry + rz*rz)
+#define R() rr
+#define RINV() 0
+#define R6() 0
+#endif
 
 // =======================================================================
 
@@ -25,11 +56,13 @@ void ekin(mdsys_t *sys)
 /* static void force(mdsys_t *sys)  */
 void force(mdsys_t *sys) 
 {
-  double r,ffac;
+  double r, rr, rrc, ffac;
   double rx,ry,rz;
   int i, j;
   double epot = 0.0;
   double * fx, *fy, *fz;
+  double c12, c6;
+  double r6, rinv;
 
   /* zero energy and forces */
 #ifdef USE_MPI
@@ -57,9 +90,16 @@ void force(mdsys_t *sys)
   azzero( fy, sys->natoms );
   azzero( fz, sys->natoms );
 
+  c12 = C12(); 
+  c6 = C6();
+  rrc = RRC();
+
   /* loop to compute forces */
+
+
+
   for( i = sys->rank; i < (sys->natoms); i += sys->npes ) {
-    for(j=0; j < (sys->natoms); ++j) {
+    for(j=J_BEGIN(); j < (sys->natoms); ++j) {
 
       /* particles have no interactions with themselves */
       if (i==j) continue;
@@ -67,19 +107,33 @@ void force(mdsys_t *sys)
       /* get distance between particle i and j */
       rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
       ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
-      rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
-      r = sqrt(rx*rx + ry*ry + rz*rz);
-      
+      rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);     
+      rr = RR();
       /* compute force and energy if within cutoff */
-      if (r < sys->rcut) {
-		ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r
-					  +6*pow(sys->sigma/r,6.0)/r);
-		
-		epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
-					      -pow(sys->sigma/r,6.0));
+	  
+      if (rr < rrc) {
+		r = R();
+		rinv=RINV(); 
+		r6 = R6(); 
+
+		ffac = FFAC();		
+		epot += HALF_FACTOR()*EPOT();
+	
+		// fx[i] += rx/r*ffac;
+		// fy[i] += ry/r*ffac;
+		// fz[i] += rz/r*ffac;
+
+
+#ifdef THIRD_LAW
+		fx[i] += rx/r*ffac; fx[j] -= rx/r*ffac;
+		fy[i] += ry/r*ffac; fy[j] -= ry/r*ffac;
+		fz[i] += rz/r*ffac; fz[j] -= rz/r*ffac;
+#else
 		fx[i] += rx/r*ffac;
 		fy[i] += ry/r*ffac;
 		fz[i] += rz/r*ffac;
+#endif
+
       }
     }
   }  
@@ -99,7 +153,8 @@ void force(mdsys_t *sys)
   /* sys->fz = fz; */
   sys->epot = epot;
 #endif //USE_MPI
-  
+
+
   return;
 }
 
